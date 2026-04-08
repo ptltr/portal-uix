342981fa13ebd84a41a517d78279044981080dfe
ptltr <paolatoledot25@gmail.com>
2026-04-08 20:07:58 +0000

    Harden Pages routes for Capital Humano deep links

--- chatbot-colaboradores/src/App.tsx
+++ chatbot-colaboradores/src/App.tsx
@@ -1,6 +1,7 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/Home";
+import CapitalHumano from "./pages/CapitalHumano";
 import NotFound from "./pages/not-found";
 
 const queryClient = new QueryClient({
@@ -16,6 +17,12 @@ function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/portal-uix" component={Home} />
+      <Route path="/portal-uix/" component={Home} />
+      <Route path="/capital-humano" component={CapitalHumano} />
+      <Route path="/capital-humano/" component={CapitalHumano} />
+      <Route path="/portal-uix/capital-humano" component={CapitalHumano} />
+      <Route path="/portal-uix/capital-humano/" component={CapitalHumano} />
       <Route component={NotFound} />
     </Switch>
   );
